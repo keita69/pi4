c74da0f7058f2f577a298339c9b90c4f621029ff
keita69sawada <keita69sawada@gmail.com>
2020-11-22 22:05:58 +0900

    create functions

--- sensor_writer.py
+++ sensor_writer.py
@@ -1,22 +1,32 @@
 # -*- coding: utf-8 -*-                                                                                                                               
-
+# refarence
+# https://qiita.com/Taroi_Japanista/items/ac51ca2f636f422cced6
 import os
 import Adafruit_DHT
 import datetime
 
-now = datetime.datetime.now() # 現在の日時を取得                                                                                                      
+def main():
+    now = datetime.datetime.now() # 現在の日時を取得
+    humidity, temperature = getHumidiyTEmperatureFromDHT22()
 
-sensor = Adafruit_DHT.DHT22
-pin = 4
+    max_range_temperature = 28.0
+    min_range_temperature = 20.0
 
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    max_range_humidity = 70.0
+    min_range_humidity = 40.0
+    writeCsv( now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity)
 
-max_range_temperature = 28.0
-min_range_temperature = 20.0
+def getHumidiyTEmperatureFromDHT22():
+    sensor = Adafruit_DHT.DHT22
+    pin = 4
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    return(humidity, temperature)
 
-max_range_humidity = 70.0
-min_range_humidity = 40.0
 
-file = open('sensor_log.csv', 'a+')  #書き込みモードでオープン                                                                           
-file.write('{0:%Y/%m/%d %H:%M:%S}, {1:3f}, {2:3f}, {3:3f}, {4:3f}, {5:3f}, {6:3f}\n'.format(now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity))
+def writeCsv( now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity):
+    file = open('sensor_log.csv', 'a+')  #書き込みモードでオープン  
+    file.write('{0:%Y/%m/%d %H:%M:%S}, {1:3f}, {2:3f}, {3:3f}, {4:3f}, {5:3f}, {6:3f}\n'.format(now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity))
+
+def writeGoogleSpreadSheet( now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity):
+    print("TODO:WIRTE GOOGOLE SPREADSHEET")
 
