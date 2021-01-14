fa82da6bfe13ad850f8970123c2d1f3f6a224001
keita69 <keita69@gmail.com>
2021-01-14 15:26:04 +0900

    update

--- line_notify.py
+++ line_notify.py
@@ -2,38 +2,19 @@ import requests
 import configparser
  
 
-def notify( now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity):
-    config = configparser.ConfigParser()
-    config.read('secrets.ini')
-     
-    access_token = config.get('line', 'access_token') 
-    
-    url = "https://notify-api.line.me/api/notify"
-    headers = {'Authorization': 'Bearer ' + access_token}
-
-    is_line_noify, message =  checkLineNotify( now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity)
-
-    if is_line_notify :
-        payload = {'message': message}
-        r = requests.post(url, headers=headers, params=payload,)
-
-
-
-
-
 def checkLineNotify( now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity):
 
     message = 'good condition!'
-    is_line_notyfy = True
+    is_line_notify = True
 
     if min_range_temperature <= temperature <= max_range_temperature :
-        message = "GOOD CONDITION!"
+        message = "GOOD CONDITION! \n[{0} c] [{1} %]".format('{:.1f}'.format(temperature), '{:.1f}'.format(humidity))
         is_line_notify = False
 
     if min_range_temperature > temperature :
-        message = 'TOO COOL!! [ {0} c] [{1} %]'.format(temperature, humidity)
+        message = 'TOO COOL!! \n[{0} c] [{1} %]'.format('{:.1f}'.format(temperature), '{:.1f}'.format(humidity))
     elif temperature > max_range_temperature :
-        message = 'TOO HOT!! [ {0} c][ {1} %]'.format(temperature, humidity)
+        message = 'TOO HOT!! \n[{0} c] [{1} %]'.format('{:.1f}'.format(temperature), '{:.1f}'.format(humidity))
 
     path_w = 'line_notify_write_last_time.txt'
 
@@ -44,8 +25,9 @@ def checkLineNotify( now, humidity, temperature, max_range_temperature, min_rang
     
     #TODO if 15分以内であれば通知しない
 
-
-
+    #is_line_notify = True
+    print('{0} [{1} c] [{2} %]'.format(now, '{:.1f}'.format(temperature), humidity))
+    print('{0} [{1} c] [{2} %] [{3}]'.format(now, temperature, humidity, is_line_notify))
 
     if is_line_notify :
         nowstr = now.strftime('%Y-%m-%d %H:%M:%S')
@@ -53,3 +35,23 @@ def checkLineNotify( now, humidity, temperature, max_range_temperature, min_rang
             f.write(nowstr)
 
     return is_line_notify, message
+
+
+
+def notify( now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity):
+    config = configparser.ConfigParser()
+    config.read('secrets.ini')
+     
+    access_token = config.get('line', 'access_token') 
+    
+    url = "https://notify-api.line.me/api/notify"
+    headers = {'Authorization': 'Bearer ' + access_token}
+
+    judge, message =  checkLineNotify( now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity)
+
+    print('{0} [{1} c] [{2} %] [{3}]'.format(now, temperature, humidity, judge))
+
+    if judge :
+        payload = {'message': message}
+        r = requests.post(url, headers=headers, params=payload,)
+
--- sensor_writer.py
+++ sensor_writer.py
@@ -33,7 +33,7 @@ def main():
 
     writeCsv( now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity)
     
-    writeGoogleSpreadSheet( now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity)
+#    writeGoogleSpreadSheet( now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity)
 
     google_spreadsheet_writer.write( now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity)
 
@@ -51,50 +51,50 @@ def writeCsv( now, humidity, temperature, max_range_temperature, min_range_tempe
     file = open('sensor_log.csv', 'a+')  #書き込みモードでオープン  
     file.write('{0:%Y/%m/%d %H:%M:%S}, {1:3f}, {2:3f}, {3:3f}, {4:3f}, {5:3f}, {6:3f}\n'.format(now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity))
 
-def writeGoogleSpreadSheet( now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity):
-
-    # If modifying these scopes, delete the file token.pickle.
-    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-
-    # The ID and range of a sample spreadsheet.
-    SPREADSHEET_ID = '1Ji2PrvuhPHWHu0K-fGaxgCvqSV8vd2AkqJDYCPcrjkA'     
-
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-        service = build('sheets', 'v4', credentials=creds)
-
-        # XXXXXXXXXX
-        sheetName = '温度と湿度'
-        rangeName = 'A:G'
-        ValueInputOption = 'USER_ENTERED'
-        body = {
-            'values': [[now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity]],
-        }
-        result = service.spreadsheets().values().append(
-            spreadsheetId=SPREADSHEET_ID, range=sheetName + "!" + rangeName,
-            valueInputOption=ValueInputOption, body=body).execute()
-        # TODO add judge result
-        pprint(result)
+# def writeGoogleSpreadSheet( now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity):
+# 
+#     # If modifying these scopes, delete the file token.pickle.
+#     SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+# 
+#     # The ID and range of a sample spreadsheet.
+#     SPREADSHEET_ID = '1Ji2PrvuhPHWHu0K-fGaxgCvqSV8vd2AkqJDYCPcrjkA'     
+# 
+#     """Shows basic usage of the Sheets API.
+#     Prints values from a sample spreadsheet.
+#     """
+#     creds = None
+#     # The file token.pickle stores the user's access and refresh tokens, and is
+#     # created automatically when the authorization flow completes for the first
+#     # time.
+#     if os.path.exists('token.pickle'):
+#         with open('token.pickle', 'rb') as token:
+#             creds = pickle.load(token)
+#     # If there are no (valid) credentials available, let the user log in.
+#     if not creds or not creds.valid:
+#         if creds and creds.expired and creds.refresh_token:
+#             creds.refresh(Request())
+#         else:
+#             flow = InstalledAppFlow.from_client_secrets_file(
+#                 'credentials.json', SCOPES)
+#             creds = flow.run_local_server(port=0)
+#         # Save the credentials for the next run
+#         with open('token.pickle', 'wb') as token:
+#             pickle.dump(creds, token)
+# 
+#         service = build('sheets', 'v4', credentials=creds)
+# 
+#         # XXXXXXXXXX
+#         sheetName = '温度と湿度'
+#         rangeName = 'A:G'
+#         ValueInputOption = 'USER_ENTERED'
+#         body = {
+#             'values': [[now, humidity, temperature, max_range_temperature, min_range_temperature, max_range_humidity, min_range_humidity]],
+#         }
+#         result = service.spreadsheets().values().append(
+#             spreadsheetId=SPREADSHEET_ID, range=sheetName + "!" + rangeName,
+#             valueInputOption=ValueInputOption, body=body).execute()
+#         # TODO add judge result
+#         pprint(result)
 
 if __name__ == '__main__':
     main()
